6c0ac788e43704fe02720afdce4424a6e1cbfe3f
Abiyu00696 <Abiyu00696@gmail.com>
2026-08-04 20:22:13 +0300

    update

--- login/login.py
+++ login/login.py
@@ -1,3 +1,5 @@
+import time
+
 user_info = {
 
     "james" : "pa55w0rd",
@@ -12,15 +14,32 @@ user_info = {
     "thomas" : "b00s",
     "admin" : "admin"
 }
+def lock(lock_time):
+    time.sleep(lock_time)
+    return
 
 running = True
 allowed_attempt = 5
-attempt = 0
+attempt = 1
+lock_time = 5
+
+print("++++ Welcome User +++++")
 while running:
-    print("++++ Welcome User +++++")
+    if attempt > allowed_attempt:
+        print("You have used all your allowd attempt")
+        print(f"Pleas wait {lock_time / 60} min. and try again")
+        lock(lock_time)
+        attempt = 0
     
     username = input("Username: ")
     passwd = input("password: ")
 
     if username in user_info and passwd in user_info[username]:
-        print("Correct")
+        print("\nCorrect credentials")
+        print("Login Succuess full")
+        running = False
+    else:
+        print(f"\nIncorrect credentials, Attemp left { (allowed_attempt - attempt )  }")
+
+
+    attempt += 1
