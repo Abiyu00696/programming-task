c73f399bed30dd8e6291c9f1554dcd8fe91c8363
Abiyu00696 <Abiyu00696@gmail.com>
2026-08-06 09:42:43 +0300

    fix some issues

--- login/login.py
+++ login/login.py
@@ -14,33 +14,49 @@ def lock(lock_time):
 running = True
 allowed_attempt = 5
 attempt = 1
-lock_time = 5
+lock_time = 60
+unlock_time = None
 
 print("++++ Welcome User +++++")
-while running:
-    if attempt > allowed_attempt:
-        current_time = datetime.now()
-        unlock_time = current_time + timedelta(seconds=lock_time)
-        print("---------------------------------------")
 
-        print("You have used all your allowd attempt\n")
-        print(f"Current time: {current_time.strftime('%H:%M:%S')}")
-        
-        print(f"Pleas wait {lock_time / 60} min until {unlock_time.strftime('%H:%M:%S')}")
-        print("System is locked")
-        s
-        lock(lock_time)
+while running:
+    if unlock_time is not None:
+        current_time = datetime.now()
+
+        if current_time < unlock_time:
+            print("---------------------------------------")
+
+            print("You have used all your allowd attempt\n")
+            print(f"Current time: {current_time.strftime('%H:%M:%S')}")
+            
+            print(f"Pleas wait {lock_time / 60} min until {unlock_time.strftime('%H:%M:%S')}")
+            print("System is locked")
+            
+        while datetime.now() < unlock_time:
+            remaining_time = unlock_time - datetime.now()
+            print(f"\rSystem locked. Please wait {remaining_time.seconds} seconds ...", end="")
+            time.sleep(1)
+        print("\nSystem unlocked. \n")
+        print("-"*25)
+        unlock_time = None
         attempt = 1
-    
+
     username = input("Username: ")
     passwd = input("password: ")
 
     if username in user_info and  user_info[username] == passwd:
         print("\nCorrect credentials")
-        print("Login Succuess full")
+        print("Login Succuessful")
         running = False
     else:
-        print(f"\nIncorrect credentials, Attemp left { (allowed_attempt - attempt )  }")
+        if attempt >= allowed_attempt: 
+            current_time = datetime.now() 
+            unlock_time = current_time + timedelta(seconds=lock_time) 
+            print("---------------------------------------") 
+            print("You have used all your allowed attempts.")
+            print(f"Current time: {current_time.strftime('%H:%M:%S')}") 
+            print("System is locked.") 
+            print(f"Unlock time: {unlock_time.strftime('%H:%M:%S')}")
+        print(f"\nIncorrect credentials, Attemp left {allowed_attempt - attempt}")
+        attempt += 1
 
-
-    attempt += 1
